97a77f262dd566fcd35bdbf98b29fa51910f94ec
Pratham <pgupta4596@gmail.com>
2022-06-09 21:22:15 +0530

    Completed

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-
+app.use(express.json({ limit: "50mb" })); 
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 
 connectDatabase();
-app.use(express.json({ limit: "50mb" })); 
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
